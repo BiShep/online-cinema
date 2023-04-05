3463ebfa77791c020ab758e46aadbecce3a22491
BiShep <eldiar44@bk.ru>
2023-04-05 12:17:01 +0600

    first commit

---
+++ exxeption.py
@@ -0,0 +1,17 @@
+# num = input('введите число от 1 до 7: ')
+#
+# if int(num) > 7 or int(num) < 1:
+#     raise Exception('error')
+# days_of_week = {
+#     '1': 'mondey',
+#     '2': 'tu',
+#     '3': 'we',
+#     '4': 'th',
+#     '5': 'fr',
+#     '6': 'sa',
+#     '7': 'sun'
+# }
+#
+# print(days_of_week[num])
+
+
